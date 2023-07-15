2ab548c84a2b889ab3fd482945023ffb6e7cad52
AlonsoQuijanoRuiz <aquijanoruiz@gmail.com>
2023-07-15 16:46:05 -0500

    Fixed some errors

--- code/wrangle.R
+++ code/wrangle.R
@@ -17,12 +17,9 @@ source("code/weather.R")
 
 # Merge cleaned survey data with weather data for the 2018 and 2012 Ensanut waves
 Weather_HealthPerceiption_data <- bind_rows(
-  left_join(survey_clean_2018, weather_clean_2018, by = "person_id") %>%  mutate(survey_wave = "ENSANUT 2018"),
+  left_join(survey_clean_2018, weather_clean_2018, by = "person_id") %>% mutate(survey_wave = "ENSANUT 2018"),
   left_join(survey_clean_2012, weather_clean_2012, by = "person_id") %>% mutate(survey_wave = "ENSANUT 2012"))
 
 # Save data as RData and dta
-save(Weather_HealthPerceiption, file = "data/Weather_HealthPerceiption_data.RData")
+save(Weather_HealthPerceiption_data, file = "data/Weather_HealthPerceiption_data.RData")
 write_dta(Weather_HealthPerceiption_data, path = "data/Weather_HealthPerceiption_data.dta")
-
-# Compress to zip file
-zip(files = "data/Weather_HealthPerceiption_data.dta", zipfile =  "data/Weather_HealthPerceiption_data.dta.zip")
