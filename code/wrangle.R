4b7a3ccee5a5d0f11c95ac255baee99e6409917b
AlonsoQuijanoRuiz <aquijanoruiz@gmail.com>
2023-07-16 16:24:27 -0500

    Fixed typos

--- code/wrangle.R
+++ code/wrangle.R
@@ -16,11 +16,11 @@ source("code/survey.R")
 source("code/weather.R")
 
 # Merge cleaned survey data with weather data for the 2018 and 2012 Ensanut waves
-Weather_HealthPerceiption_2018data <- left_join(survey_clean_2018, weather_clean_2018, by = "person_id")
-Weather_HealthPerceiption_2012data <- left_join(survey_clean_2012, weather_clean_2012, by = "person_id")
+Weather_HealthPerception_2018data <- left_join(survey_clean_2018, weather_clean_2018, by = "person_id")
+Weather_HealthPerception_2012data <- left_join(survey_clean_2012, weather_clean_2012, by = "person_id")
 
 # Save data as RData and dta
-save(Weather_HealthPerceiption_2018data, file = "data/Weather_HealthPerceiption_2018data.RData")
-write_dta(Weather_HealthPerceiption_2018data, path = "data/Weather_HealthPerceiption_2018data.dta")
-save(Weather_HealthPerceiption_2012data, file = "data/Weather_HealthPerceiption_2012data.RData")
-write_dta(Weather_HealthPerceiption_2012data, path = "data/Weather_HealthPerceiption_2012data.dta")
+save(Weather_HealthPerception_2018data, file = "data/Weather_HealthPerception_2018data.RData")
+write_dta(Weather_HealthPerception_2018data, path = "data/Weather_HealthPerception_2018data.dta")
+save(Weather_HealthPerception_2012data, file = "data/Weather_HealthPerception_2012data.RData")
+write_dta(Weather_HealthPerception_2012data, path = "data/Weather_HealthPerception_2012data.dta")
